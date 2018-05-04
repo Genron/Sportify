f08807dce771dca0273e520110a929a35d975bbf
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2018-05-04 16:24:33 +0200

    POC läuft

--- src/providers/firebase-service/firebase-service.ts
+++ src/providers/firebase-service/firebase-service.ts
@@ -15,13 +15,15 @@ import {Observable} from 'rxjs/Observable';
 export class FirebaseServiceProvider {
 
   gamesRef: AngularFireList<any>;
+  teamsRef: AngularFireList<any>;
   games: Observable<any[]>;
 
   constructor(public afd: AngularFireDatabase) {
-    this.gamesRef = this.afd.list('/weneedItems/');
+    this.gamesRef = this.afd.list('/games/');
     this.games = this.gamesRef.snapshotChanges().map(changes => {
       return changes.map(c => ({key: c.payload.key, ...c.payload.val()}));
     });
+
   }
 
   getGames() {
@@ -35,7 +37,9 @@ export class FirebaseServiceProvider {
   addTeam(selectedGame, newName) {
     // TODO: Add the Team to the Game
     // return this.gamesRef.push({value: newName, isDone: false, teams: []});
-    return this.gamesRef.push(null);
+    this.teamsRef = this.afd.list('/games/' + selectedGame.key + '/');
+
+    return this.teamsRef.push({teamName: newName});
   }
 
   updateGame(key, newGameName) {
