cd6dca7ac2225b370650e1139f0a4f32ccdac943
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2018-05-28 14:23:58 +0200

    opponents and myTeam are now accessible.

--- src/pages/versus/versus.ts
+++ src/pages/versus/versus.ts
@@ -16,13 +16,33 @@ import {Observable} from "rxjs/Observable";
 })
 export class VersusPage {
   teams: Observable<any[]>;
+  team: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.teams = navParams.get("attendingTeams");
+    this.teams.subscribe(
+      x => this.matchTeams(x),
+      e => console.log('onError: %s', e),
+      () => console.log('onCompleted')
+    );
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VersusPage');
+  }
+
+  matchTeams(allTeams){
+    for (let i = 0; i < allTeams.length ; i++) {
+    // console.log("Anzahl: " + allTeams.length + " Teams: " + allTeams[i].teamName);
+      let opponents = allTeams.slice(); //Create a copy of the teams
+      // console.log("Anzahl: " + opponents.length + " Teams: " + opponents[i].teamName);
+      let myTeam = opponents.splice(i, 1)[0]; //Cut my team out of opponents => opponents has now all other teams except this team
+      // console.log("Anzahl: " + opponents.length + " Teams: " + opponents[0].teamName);
+      console.log(opponents);
+      console.log(myTeam.teamName);
+    }
+
   }
 
 }
